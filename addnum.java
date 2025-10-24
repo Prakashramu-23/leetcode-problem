0dad26e96008aa7ddfb552f8cb6d4807d3531dad
Prakashramu-23 <prakashmechselvam@gmail.com>
2025-10-24 22:28:11 +0530

    add

---
+++ addnum.java
@@ -0,0 +1,9 @@
+public class addnum {
+    public static void main(String[] args) {
+        int i=9;
+        int j=99;    
+        System.out.println("The sum is: " + (i+j));
+        System.out.println("The difference is: " + (i-j));
+    }
+    
+}
